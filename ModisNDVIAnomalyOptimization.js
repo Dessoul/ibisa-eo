87aa774d15bc37e387c4b42d5c9fde4d797fc069
ILoomans <50031945+ILoomans@users.noreply.github.com>
2020-04-06 13:59:14 +0200

    Update ModisNDVIAnomalyOptimization.js

--- ModisNDVIAnomalyOptimization.js
+++ ModisNDVIAnomalyOptimization.js
@@ -123,7 +123,7 @@ function setup(dss) {
 function filterScenes(scenes, metadataInput) {
 	
 	filteredScenes = [];
-	for (i=0; i < scenes.length ; i++)
+	for (var i=0; i < scenes.length ; i++)
 	{
 		if (scene[i].date.getMonth()===metadataInput.to.getMonth() && scene[i].date.getFullYear() >= metadataInput.to.getFullYear() - nbPastYears) 
 		{
