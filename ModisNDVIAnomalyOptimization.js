a2c2d3f121168d58836be5455f4fd4d25877971d
ILoomans <50031945+ILoomans@users.noreply.github.com>
2020-04-06 10:54:31 +0200

    Update ModisNDVIAnomalyOptimization.js

--- ModisNDVIAnomalyOptimization.js
+++ ModisNDVIAnomalyOptimization.js
@@ -127,7 +127,7 @@ function filterScenes(scenes, metadataInput) {
   }
   throw new Error(tmpString)*/
   //throw new Error(scenes)
-
+  console.log(scene)
   return scenes.filter(function(scene) {return (scene.date.getMonth() === metadataInput.to.getMonth() && scene.date.getFullYear() >= metadataInput.to.getFullYear() - nbPastYears) ; }) ;
 } ;
 
